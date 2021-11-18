91a5b17d73b8c2343dc4809cdd66e1f6a114114c
SaraAleksoska <72557477+SaraAleksoska@users.noreply.github.com>
2021-11-18 13:43:52 +0100

    Delete novoAvtorKniga directory

--- novoAvtorKniga/src/main/java/novo/novoAvtorKniga/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package novo.novoAvtorKniga.repositories;
-
-import novo.novoAvtorKniga.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- novoAvtorKniga/src/main/java/novo/novoAvtorKniga/repositories/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package novo.novoAvtorKniga.repositories;
-
-import novo.novoAvtorKniga.domain.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
